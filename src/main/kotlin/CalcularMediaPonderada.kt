c0f73396f7dbb4333c757600801503f1bfd2edce
Roberto <rober.autom@gmail.com>
2022-09-11 11:55:10 -0300

    Initial commit

---
+++ src/main/kotlin/CalcularMediaPonderada.kt
@@ -0,0 +1,33 @@
+/**
+ * Este programa cálcula a média ponderada
+ * entre três valores
+ * nota1, nota2, nota3
+ * peso1, peso2, peso3
+ * peso1 = 2
+ * peso2 = 3
+ * peso3 = 4
+ *
+ * mediaPonderada = ((nota1*peso1) + (nota2*peso2) + (nota3*peso3)) / (peso1+peso2+peso3)
+ *
+ */
+
+fun main() {
+
+    val peso1:Int = 2
+    val peso2:Int = 3
+    val peso3:Int = 4
+
+    println("Digite a primeira nota:")
+    val nota1:Double = readLine()!!.toDouble()
+
+    println("Digite a segunda nota:")
+    val nota2:Double = readLine()!!.toDouble()
+
+    println("Digite a terceira nota:")
+    val nota3:Double = readLine()!!.toDouble()
+
+    var mediaPonderada =
+        ((nota1*peso1)+(nota2*peso2)+(nota3*peso3))/(peso1+peso2+peso3)
+
+    println("Média Ponderada Calculada: $mediaPonderada")
+}
